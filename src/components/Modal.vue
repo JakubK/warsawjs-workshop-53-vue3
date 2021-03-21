84ee9149a7a0771955595e0c7a73f4b29c4285ed
JakubK <jakubkuniszewski1@gmail.com>
2021-03-21 15:21:35 +0100

    Wrap Moda component with Teleport to body and emit update on cross click

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,20 +1,23 @@
 <template>
+    <Teleport to="body">
   <div class="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50">
     <!-- modal -->
-    <div class="bg-white rounded shadow-lg w-10/12 md:w-1/3">
+    <div class="bg-white rounded shadow-lg">
       <!-- modal header -->
       <div class="border-b px-4 py-2 flex justify-between items-center">
-        <h3 class="font-semibold text-lg">Modal Title</h3>
-        <button class="text-black close-modal">&cross;</button>
+        <h3 class="font-semibold text-lg">{{ title }}</h3>
+        <button @click="$emit('update:active', false)" class="text-black close-modal">&cross;</button>
       </div>
       <!-- modal body -->
-      <div class="p-6">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Rerum, delectus cumque fugiat nemo ducimus quae deserunt
-        cupiditate sapiente incidunt aut accusantium dolore assumenda
-        vitae similique, exercitationem voluptatum praesentium
-        laboriosam nam.
-      </div>
+      <slot class="p-6">
+      </slot>
     </div>
   </div>
+    </Teleport>
 </template>
+<script>
+export default {
+    props: ['title', 'active'],
+    emits: ['update:active'],
+}
+</script>
